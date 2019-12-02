14bf589a80070df0ff51607cdb64009314493746
Marteinneli <marteinneli13@gmail.com>
2019-12-02 17:57:53 +0000

    Routes updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-SCREENLENGTH = 40
+SCREENLENGTH = 60
 BACK_STR = "B - Back"
 QUIT_STR = "Q - Quit"
 
@@ -44,13 +44,22 @@ file_stream = open_file(file_name)
 # display_crew = create_crew_list(file_stream)
     
 def startScreen():
+    #valid_inputs = 123 q b
     choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
-    choice_dict = {"1" : employeeScreen, "2" : airplaneScreen, "3" : airplaneScreen, "Q" : exit}
+    choice_dict = {"1" : employeeScreen, "2" : airplaneScreen, "3" : tripAndLocScreen, "Q" : exit}
+    display(choice_list)
+    inp = askForInput()
+    #Check if input is valid
+    choice_dict.get(inp)()
+
+def employeeScreen():
+    choice_list = ["1 - Add Employee", "2 - Show Employee", "3 - Edit Employee", QUIT_STR]
+    choice_dict = {"1" : addEmployeeScreen, "2" : showEmpScreen, "3" : editEmpScreen, "Q" : exit}
     display(choice_list)
     inp = askForInput()
     choice_dict.get(inp)()
 
-def employeeScreen():
+def addEmployeeScreen():
     while True:
         choice_list = ["1 - Add Misc Employee", "2 - Add Pilot", "3 - Add Flight Attendant", BACK_STR, QUIT_STR]
         choice_dict = {"1": addMiscEmpScreen, "2" : addPilotScreen, "3" : addAttendantScreen, "B" : back, "Q" : exit}
@@ -83,115 +92,116 @@ def addAttendantScreen():
     pass
 
 def showEmpScreen():
-    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
-    choice_dict = {}
+    choice_list = ["1 - Employees", "2 - Pilots", "3 - Flight Attendants", QUIT_STR]
+    choice_dict = {"1": miscFilterScreen, "2" : pilotFilterScreen, "3" : attFilterScreen, "B" : back, "Q" : exit}
     display(choice_list)
     inp = askForInput()
-    choice_dict.get(inp)
-    pass
+    choice_dict.get(inp)()
+    
     
 def editEmpScreen():
-    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
-    choice_dict = {}
+    choice_list = ["1 - Edit Any Employee", "2 - Edit Pilot", "3 - Edit Attendant", QUIT_STR]
+    choice_dict = {"1" : whileEditingScreen, "2" : whileEditingScreen, "3" : whileEditingScreen}
     display(choice_list)
     inp = askForInput()
-    choice_dict.get(inp)
-    pass
+    choice_dict.get(inp)()
+    
     
 def miscFilterScreen():
-    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
-    choice_dict = {}
+    choice_list = ["1 - Show All Employees", "2 - Show Employees at work on day/week", "3 - Show Employees not at work on day/week", "4 - Look for Employee bt SSN", QUIT_STR]
+    choice_dict = {"1" : atWorkScreen , "2" : atWorkScreen, "3" : notAtWorkScreen, "4" : atWorkScreen }
     display(choice_list)
     inp = askForInput()
-    choice_dict.get(inp)
-    pass
+    choice_dict.get(inp)()
+    
     
 def attFilterScreen():
-    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
-    choice_dict = {}
+    choice_list = ["1 - Show All Attendants", "2 - Show Attendants at work on day/week", "3 - Show Attendants not at work on day/week", "4 - Look for Attendant bt SSN", QUIT_STR]
+    choice_dict = {"1" : atWorkScreen , "2" : atWorkScreen, "3" : notAtWorkScreen, "4" : atWorkScreen }
     display(choice_list)
     inp = askForInput()
-    choice_dict.get(inp)
-    pass
+    choice_dict.get(inp)()
+    
     
 def pilotFilterScreen():
-    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
-    choice_dict = {}
+    choice_list = ["1 - Show All Pilots", "2 - Show pilots at work on day/week", "3 - Show pilots not at work on day/week", "4 - Look for pilot bt SSN", QUIT_STR]
+    choice_dict = {"1" : atWorkScreen , "2" : atWorkScreen, "3" : notAtWorkScreen, "4" : atWorkScreen }
     display(choice_list)
     inp = askForInput()
-    choice_dict.get(inp)
-    pass
+    choice_dict.get(inp)()
+    
     
 def atWorkScreen():
-    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_list = ["1 - Show By Day", "2 - Show By Week", QUIT_STR]
     choice_dict = {}
     display(choice_list)
     inp = askForInput()
     choice_dict.get(inp)
-    pass
+    
     
 def notAtWorkScreen():
-    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_list = ["1 - Show By Day", "2 - Show By Week", QUIT_STR]
     choice_dict = {}
     display(choice_list)
     inp = askForInput()
     choice_dict.get(inp)
-    pass
+    
     
 def airplaneScreen():
-    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_list = ["1 - Show All Airplanes", "2 - Add Airplanes", "3 - Show Pilots Sorted by plane make", "4 - Show Pilots For Specific Plane Make", QUIT_STR]
     choice_dict = {}
     display(choice_list)
     inp = askForInput()
     choice_dict.get(inp)
-    pass
+    
     
 def tripAndLocScreen():
-    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
-    choice_dict = {}
+    choice_list = ["1 - Locations", "2 - Work Trips", QUIT_STR]
+    choice_dict = {"1" : locationScreen, "2" : workTripsScreen}
     display(choice_list)
     inp = askForInput()
-    choice_dict.get(inp)
-    pass
+    choice_dict.get(inp)()
+    
     
 def workTripsScreen():
-    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
-    choice_dict = {}
+    choice_list = ["1 - Add Work Trip", "2 - Show Work Trips", "3 - Show An Employee's Work Trip", QUIT_STR]
+    choice_dict = {"2" : filterWorkTripsScreen}
     display(choice_list)
     inp = askForInput()
-    choice_dict.get(inp)
-    pass
+    choice_dict.get(inp)()
+    
     
 def locationScreen():
-    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_list = ["1 - Add Location", "2 - Edit Location", "3 - Show All Locations", "4 - Show Most Popular Location", QUIT_STR]
     choice_dict = {}
     display(choice_list)
     inp = askForInput()
     choice_dict.get(inp)
-    pass
+    
     
 def whileEditingScreen():
-    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_list = ["1 - Change Home Address", "2 - Change Phone Number", "3 - Change Email", "4 - Change Plane Type", QUIT_STR]
     choice_dict = {}
     display(choice_list)
     inp = askForInput()
-    choice_dict.get(inp)
-    pass
+    choice_dict.get(inp)()
+    
     
 def phoneEditScreen():
-    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_list = ["1 - Change Home Phone Number", "2 - Change Mobile Number", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice_dict.get(inp)
+
+    
+def filterWorkTripsScreen():
+    choice_list = ["1 - Show All Trips", "2 - Show For Day", "3 - Show For Week", QUIT_STR]
     choice_dict = {}
     display(choice_list)
     inp = askForInput()
     choice_dict.get(inp)
     pass
-    
-def filterWorkTripsScreen():
-    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
-    choice_dict = {}
-    display(choice_list)
-    inp = askForInput()
-    choice_dict.get(inp)
     
 def dayWeekScreen():
     choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
