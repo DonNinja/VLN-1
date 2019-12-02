3f2013b42a13fe60d109e168f42549e334901076
Andres <andresp18@ru.is>
2019-12-02 15:15:13 +0000

    prufa def open_file

--- main.py
+++ main.py
@@ -20,10 +20,21 @@ def display(choice_list):
         print(line)
     printLine()
 
+def open_file(file_name):
+    try:
+        file_stream = open(file_name, "r")
+        return file_stream
+    except FileNotFoundError:
+        return None
+
+file_name = "crew.csv"
+file_stream = open_file(file_name)
+
 
 choice_list = ["1 - Single Kek", "2 - Double kek",
                "3 - Triple kek", BACK_STR, QUIT_STR]
 
 display(choice_list)
+ #geggjað comment
 
-# Böddi löpp
+#hello world
