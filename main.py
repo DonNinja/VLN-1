409a701fdbb864cfe570d796abe20275218ca742
Yngvi <yngvileo@gmail.com>
2019-12-02 15:43:10 +0000

    Kom upp fullt af föllum

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ SCREENLENGTH = 40
 BACK_STR = "B - Back"
 QUIT_STR = "Q - Quit"
 
+def askForInput():
+    return input("Enter a choice: ")
+
+def back():
+    return
 
 def printLine():
     line = ""
@@ -9,6 +14,8 @@ def printLine():
         line += "="
     print(line)
 
+def choice(choice_dict, inp):
+    return choice_dict.get()
 
 def display(choice_list):
     printLine()
@@ -19,11 +26,147 @@ def display(choice_list):
         line += "|"
         print(line)
     printLine()
+    
+def startScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {1 : employeeScreen(), 2 : airplaneScreen(), 3 : airplaneScreen(), "Q" : exit()}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
 
+def employeeScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {1 : employeeScreen(), 2 : airplaneScreen(), 3 : airplaneScreen(), "Q" : exit()}
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
 
-choice_list = ["1 - Single Kek", "2 - Double kek",
-               "3 - Triple kek", BACK_STR, QUIT_STR]
+def addEmpScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
 
-display(choice_list)
+def showEmpScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
+    
+def editEmpScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
+    
+def miscFilterScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
+    
+def attFilterScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
+    
+def pilotFilterScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
+    
+def atWorkScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
+    
+def notAtWorkScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
+    
+def airplaneScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
+    
+def tripAndLocScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
+    
+def workTripsScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
+    
+def locationScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
+    
+def whileEditingScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
+    
+def phoneEditScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    pass
+    
+def filterWorkTripsScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
+    
+def dayWeekScreen():
+    choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
+    choice_dict = {}
+    display(choice_list)
+    inp = askForInput()
+    choice(choice_dict, inp)
 
-# Böddi löpp
+startScreen()
