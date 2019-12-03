2865dad422b11f9bfbd196ef080a65e33f7ae271
Yngvi <yngvileo@gmail.com>
2019-12-03 10:17:53 +0000

    Test push

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import os
+import sys
+
 SCREENLENGTH = 60
 BACK_STR = "B - Back"
 QUIT_STR = "Q - Quit"
+
+def restart():
+    os.execl(sys.executable, sys.executable, * sys.argv)
+
+#Hitler was right
 
 def askForInput():
     return input("Enter a choice: ").upper()
@@ -77,6 +85,7 @@ def input_check(inp,choice_dict):
             return True
 
 def startScreen():
+    
     headerDisplay("Starting screen")
     #valid_inputs = 123 q
     choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
@@ -92,6 +101,7 @@ def startScreen():
         # startScreen()
 
 def employeeScreen():
+    headerDisplay("Employee screen")
     choice_list = ["1 - Add Employee", "2 - Show Employee", "3 - Edit Employee", BACK_STR, QUIT_STR]
     choice_dict = {"1" : addEmployeeScreen, "2" : showEmpScreen, "3" : editEmpScreen, "B" : back, "Q" : exit}
     display(choice_list)
@@ -99,14 +109,15 @@ def employeeScreen():
     choice_dict.get(inp)()
 
 def addEmployeeScreen():
-    while True:
-        choice_list = ["1 - Add Misc Employee", "2 - Add Pilot", "3 - Add Flight Attendant", BACK_STR, QUIT_STR]
-        choice_dict = {"1": addMiscEmpScreen, "2" : addPilotScreen, "3" : addAttendantScreen, "B" : back, "Q" : exit}
-        display(choice_list)
-        inp = askForInput()
-        choice_dict.get(inp)()
+    headerDisplay("Add employee screen")
+    choice_list = ["1 - Add Misc Employee", "2 - Add Pilot", "3 - Add Flight Attendant", BACK_STR,QUIT_STR]
+    choice_dict = {"1": addMiscEmpScreen, "2" : addPilotScreen, "3" : addAttendantScreen, "B" : back,"Q" : exit}
+    display(choice_list)
+    inp = askForInput()
+    choice_dict.get(inp)()
 
 def addMiscEmpScreen():
+    headerDisplay(" screen")
     choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", BACK_STR, QUIT_STR]
     choice_dict = {"1" : None, "2" : None, "3" : None, "B" : back, "Q" : exit}
     display(choice_list)
@@ -128,6 +139,7 @@ def addAttendantScreen():
     choice_dict.get(inp)
 
 def showEmpScreen():
+    headerDisplay("Show employees screen")
     choice_list = ["1 - Employees", "2 - Pilots", "3 - Flight Attendants", BACK_STR, QUIT_STR]
     choice_dict = {"1": miscFilterScreen, "2" : pilotFilterScreen, "3" : attFilterScreen, "B" : back, "Q" : exit}
     display(choice_list)
@@ -136,6 +148,7 @@ def showEmpScreen():
     
     
 def editEmpScreen():
+    headerDisplay("Edit employees screen")
     choice_list = ["1 - Edit Any Employee", "2 - Edit Pilot", "3 - Edit Attendant", BACK_STR, QUIT_STR]
     choice_dict = {"1" : whileEditingScreen, "2" : whileEditingScreen, "3" : whileEditingScreen, "B" : back, "Q" : exit}
     display(choice_list)
@@ -144,6 +157,7 @@ def editEmpScreen():
     
     
 def miscFilterScreen():
+    headerDisplay("Filter employee screen")
     choice_list = ["1 - Show All Employees", "2 - Show Employees at work on day/week", "3 - Show Employees not at work on day/week", "4 - Look for Employee bt SSN", BACK_STR, QUIT_STR]
     choice_dict = {"1" : atWorkScreen , "2" : atWorkScreen, "3" : notAtWorkScreen, "4" : atWorkScreen, "B" : back, "Q" : exit}
     display(choice_list)
@@ -152,6 +166,7 @@ def miscFilterScreen():
     
     
 def attFilterScreen():
+    headerDisplay("Filter flight attendant screen")
     choice_list = ["1 - Show All Attendants", "2 - Show Attendants at work on day/week", "3 - Show Attendants not at work on day/week", "4 - Look for Attendant bt SSN", BACK_STR, QUIT_STR]
     choice_dict = {"1" : atWorkScreen , "2" : atWorkScreen, "3" : notAtWorkScreen, "4" : atWorkScreen, "B" : back, "Q" : exit}
     display(choice_list)
@@ -160,6 +175,7 @@ def attFilterScreen():
     
     
 def pilotFilterScreen():
+    headerDisplay("Filter pilot screen")
     choice_list = ["1 - Show All Pilots", "2 - Show pilots at work on day/week", "3 - Show pilots not at work on day/week", "4 - Look for pilot bt SSN", BACK_STR, QUIT_STR]
     choice_dict = {"1" : atWorkScreen , "2" : atWorkScreen, "3" : notAtWorkScreen, "4" : atWorkScreen, "B" : back, "Q" : exit}
     display(choice_list)
@@ -168,6 +184,7 @@ def pilotFilterScreen():
     
     
 def atWorkScreen():
+    headerDisplay("At work screen")
     choice_list = ["1 - Show By Day", "2 - Show By Week", QUIT_STR]
     choice_dict = {}
     display(choice_list)
@@ -176,6 +193,7 @@ def atWorkScreen():
     
     
 def notAtWorkScreen():
+    headerDisplay("Not at work screen")
     choice_list = ["1 - Show By Day", "2 - Show By Week", QUIT_STR]
     choice_dict = {"1" : None, "2" : None, "3" : None, "4" : None, "B" : back, "Q" : exit}
     display(choice_list)
@@ -184,6 +202,7 @@ def notAtWorkScreen():
     
     
 def airplaneScreen():
+    headerDisplay("Airplane screen")
     choice_list = ["1 - Show All Airplanes", "2 - Add Airplanes", "3 - Show Pilots Sorted by plane make", "4 - Show Pilots For Specific Plane Make", QUIT_STR]
     choice_dict = {"1" : None, "2" : None, "3" : None, "4" : None, "B" : back, "Q" : exit}
     display(choice_list)
@@ -192,6 +211,7 @@ def airplaneScreen():
     
     
 def tripAndLocScreen():
+    headerDisplay("Trips and locations screen")
     choice_list = ["1 - Locations", "2 - Work Trips", QUIT_STR]
     choice_dict = {"1" : locationScreen, "2" : workTripsScreen, "B" : back, "Q" : exit}
     display(choice_list)
@@ -200,6 +220,7 @@ def tripAndLocScreen():
     
     
 def workTripsScreen():
+    headerDisplay("Work trips screen")
     choice_list = ["1 - Add Work Trip", "2 - Show Work Trips", "3 - Show An Employee's Work Trip", QUIT_STR]
     choice_dict = {"1" : None, "2" : None, "3" : None, "B" : back, "Q" : exit}
     display(choice_list)
@@ -208,6 +229,7 @@ def workTripsScreen():
     
     
 def locationScreen():
+    headerDisplay("Locations screen")
     choice_list = ["1 - Add Location", "2 - Edit Location", "3 - Show All Locations", "4 - Show Most Popular Location", QUIT_STR]
     choice_dict = {"1" : None, "2" : None, "3" : None, "4" : None, "B" : back, "Q" : exit}
     display(choice_list)
@@ -216,6 +238,7 @@ def locationScreen():
     
     
 def whileEditingScreen():
+    headerDisplay("Editing employee screen")
     choice_list = ["1 - Change Home Address", "2 - Change Phone Number", "3 - Change Email", "4 - Change Plane Type", QUIT_STR]
     choice_dict = {"1" : None, "2" : None, "3" : None, "4" : None, "B" : back, "Q" : exit}
     display(choice_list)
@@ -224,6 +247,7 @@ def whileEditingScreen():
     
     
 def phoneEditScreen():
+    headerDisplay("Editing employee phone screen")
     choice_list = ["1 - Change Home Phone Number", "2 - Change Mobile Number", BACK_STR, QUIT_STR]
     choice_dict = {"1" : None, "2" : None, "B" : back, "Q" : exit}
     display(choice_list)
@@ -232,6 +256,7 @@ def phoneEditScreen():
 
     
 def filterWorkTripsScreen():
+    headerDisplay("Filter work trips screen")
     choice_list = ["1 - Show All Trips", "2 - Show For Day", "3 - Show For Week", QUIT_STR]
     choice_dict = {"1" : None, "2" : None, "3" : None, "B" : back, "Q" : exit}
     display(choice_list)
@@ -239,6 +264,7 @@ def filterWorkTripsScreen():
     choice_dict.get(inp)
     
 def dayWeekScreen():
+    headerDisplay("Day/Week screen")
     choice_list = ["1 - Employees", "2 - Airplanes", "3 - Trips and locations", QUIT_STR]
     choice_dict = {"1" : None, "2" : None, "B" : back, "Q" : exit}
     display(choice_list)
