2a72945f27b55941a8fd58aee41d49a69f5a7009
Yngvi <yngvileo@gmail.com>
2019-12-05 10:43:06 +0000

    Gerði rammann ennþá betri

--- UILayer/UIPrinter.py
+++ UILayer/UIPrinter.py
@@ -5,10 +5,10 @@ class UIPrinter:
         pass
 
     def printUpperLine(self):
-        line = "╔"
+        line = "╟"
         while len(line) < SCREENLENGTH-1:
-            line += "═"
-        line += "╗"
+            line += "─"
+        line += "╢"
         print(line)
     
     def printLowerLine(self):
@@ -19,31 +19,33 @@ class UIPrinter:
         print(line)
 
     def printHeaderUpperLine(self):
-        line = "┌"
+        print()
+        line = "╔"
         while len(line) < SCREENLENGTH-1:
-            line += "─"
-        line += "┐"
+            line += "═"
+        line += "╗"
         print(line)
         
     def printHeaderUnderLine(self):
-        line = "└"
-        while len(line) < SCREENLENGTH-1:
-            line += "─"
-        line += "┘"
-        print(line)
+        # line = "╚"
+        # while len(line) < SCREENLENGTH-1:
+        #     line += "═"
+        # line += "╝"
+        # print(line)
+        pass
 
     def headerDisplay(self, screen_name):
         self.printHeaderUpperLine()
         half_screen_length = round(SCREENLENGTH/2, 0)
         half_screen_name_length = round(len(screen_name)/2, 0)
         screen_length_center = half_screen_length - half_screen_name_length
-        line = "|"
+        line = "║"
         while len(line) < screen_length_center:
             line += " "
         line += screen_name
         while len(line) < SCREENLENGTH-1:
             line += " "
-        line += "|"
+        line += "║"
         print(line)
         self.printHeaderUnderLine()
 
