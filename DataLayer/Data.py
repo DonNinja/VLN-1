447c72be4c564b26cc4781fee9c3aedafcd224e6
SILLI20 <[sigvaldi19@ru.is]>
2019-12-09 09:51:51 +0000

    pulling

--- DataLayer/Data.py
+++ DataLayer/Data.py
@@ -1,6 +1,6 @@
 class DataAPI:
     def __init__(self):
-        self.file_stream = open("STUDENTDATA/Crew.csv", "r")
+        self.file_stream = open("VLN-1/STUDENTDATA/Crew.csv", "r")
 
     def getEmps(self):
         all_emp_list = []
@@ -9,3 +9,18 @@ class DataAPI:
             all_emp_list.append(emp_data_list)
         return all_emp_list
 
+def data_check(data,num):
+    filename = "Crew.csv"
+  
+    fields = [] 
+    rows = [] 
+  
+    with open(filename, 'r') as csvfile: 
+        csvreader = csv.reader(csvfile) 
+        fields = next(csvreader)
+        for row in csvreader: 
+            rows.append(row) 
+
+        for row in rows:
+            if row[num] == data:
+                return True
