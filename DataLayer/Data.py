1531acebf445ff250481f3c11bf68d15db7a42f4
Yngvi <yngvileo@gmail.com>
2019-12-09 14:44:46 +0000

    Fixed Data.py get operations

--- DataLayer/Data.py
+++ DataLayer/Data.py
@@ -7,19 +7,12 @@ from ModelClasses.WorkTripModel import WorkTrip
 
 class DataAPI:
     def __init__(self):
-        self.file_stream_crew = open("STUDENTDATA\\Crew.csv", "r")
-        self.file_stream_airplanes = open("STUDENTDATA\\Aircraft.csv", "r")
-    def getEmps(self):
-        all_emp_list = []
-        for line in self.file_stream_crew:
-            emp_data_list = line.split(",")
-            all_emp_list.append(emp_data_list)
-        return all_emp_list
+        self.__data = []
+        self.__reader = ReadData()
+        pass
 
+    def getEmps(self):
+        return self.__reader.readCrew()
+    
     def getAirplanes(self):
-        all_airplanes_list = []
-        for line in self.file_stream_airplanes:
-            airplane_data_list = line.split(",")
-            all_airplanes_list.append(airplane_data_list)
-        return all_airplanes_list
-        
+        return self.__reader.readAircraft()
