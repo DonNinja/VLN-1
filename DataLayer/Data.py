ac97c6fec3071401eff19daa56faa2a1ca0eba81
SILLI20 <[sigvaldi19@ru.is]>
2019-12-09 09:38:56 +0000

    pulling

--- DataLayer/Data.py
+++ DataLayer/Data.py
@@ -1,6 +1,6 @@
 class DataAPI:
     def __init__(self):
-        self.file_stream = open("STUDENTDATA\\Crew.csv", "r")
+        self.file_stream = open("STUDENTDATA/Crew.csv", "r")
 
     def getEmps(self):
         all_emp_list = []
@@ -8,3 +8,4 @@ class DataAPI:
             emp_data_list = line.split(",")
             all_emp_list.append(emp_data_list)
         return all_emp_list
+
--- LogicLayer/UserInputCheck.py
+++ LogicLayer/UserInputCheck.py
@@ -3,10 +3,44 @@ from LogicLayer.Logic import LogicAPI
 class UserInputCheck:
     def __init__(self):
         self.__data_checker = LogicAPI()
-        pass
+        self.__ssn = self.checkSSN(ssn)
 
-    def empDataInput(self, emp_type=""):
-        pass
+    def checkSSN(self, ssn):
+        status = True
+        while status == True:
+            check_int = self.digit_ssn(ssn)
+            check_len = self.len_ssn(check_int)
+            check_date = self.date_check_ssn(ssn)
+            if check_int and check_len and check_date:
+                status = False
+                return ssn
+
+
+
+    def digit_check(data):
+        try:
+            ssn = [str(int(i)) for i in ssn.split()]
+            return True
+        except:
+            ValueError
+            print('Error, only numbers allowed')
+            return False
+
+    def len_ssn(check):
+        if len(check) == 10:
+            return True
+
+    def date_check_ssn(ssn):
+        #ssn_ints = [int(i) for i in ssn]
+        #dates = ssn_ints[0] + ssn_ints[1]
+        #month = ssn_ints[2] + ssn_ints[3]
+        if re.match(r"^[0-7]\d[01]\d{3}[-]*\d{3}[09]$", ssn):
+            print("SSN is valid")
+            return True
+        else:
+            print("SSN is invalid")
+            return False
+
 
     def addLocationCountry(self):
         pass
