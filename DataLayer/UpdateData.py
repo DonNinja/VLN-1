4a78599f826db899f206b20535fe52744915a2cb
Yngvi <yngvileo@gmail.com>
2019-12-10 12:10:13 +0000

    lagaði file path

--- DataLayer/UpdateData.py
+++ DataLayer/UpdateData.py
@@ -6,7 +6,7 @@ class UpdateData:
 
     def updateEmployee(self, data):
         ''' This writes every value of the data into the crew file again, updating it. '''
-        with open("C:\\HR\\Fyrsta_onn\\VLN-1\\STUDENTDATA\\Crew.csv", 'w',newline='\n') as newFile:
+        with open("./STUDENTDATA/Crew.csv", 'w',newline='\n') as newFile:
             newFileWriter = csv.writer(newFile)
             newFileWriter.writerow(['ssn', 'name', 'role', 'rank', 'licence', 'address', 'mobilephonenumber', 'homephonenumber'])
             for item in data:
@@ -18,5 +18,5 @@ class UpdateData:
                 self.address = item['address']
                 self.mobile_phone_number = item['mobilephonenumber']
                 self.home_phone_number = item['homephonenumber']
-
+                
                 newFileWriter.writerow([self.ssn,self.name,self.role,self.rank,self.licence,self.address,self.mobile_phone_number,self.home_phone_number])
