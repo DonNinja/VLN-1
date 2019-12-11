1a981b79b1ce8d85d6e795c57c19ddeb07227183
Yngvi <yngvileo@gmail.com>
2019-12-11 10:18:07 +0000

    Fixed update employee

--- DataLayer/UpdateData.py
+++ DataLayer/UpdateData.py
@@ -8,15 +8,16 @@ class UpdateData:
         ''' This writes every value of the data into the crew file again, updating it. '''
         with open("./STUDENTDATA/Crew.csv", 'w',newline='\n') as newFile:
             newFileWriter = csv.writer(newFile)
-            newFileWriter.writerow(['ssn', 'name', 'role', 'rank', 'licence', 'address', 'mobilephonenumber', 'homephonenumber'])
+            newFileWriter.writerow(['ssn', 'name', 'role', 'rank', 'email', 'licence', 'address', 'mobilephonenumber', 'homephonenumber'])
             for item in data:
                 self.ssn = item['ssn']
                 self.name = item['name']
                 self.role = item['role']
                 self.rank = item['rank']
+                self.email = item['email']
                 self.licence = item['licence']
                 self.address = item['address']
                 self.mobile_phone_number = item['mobilephonenumber']
                 self.home_phone_number = item['homephonenumber']
                 
-                newFileWriter.writerow([self.ssn,self.name,self.role,self.rank,self.licence,self.address,self.mobile_phone_number,self.home_phone_number])
+                newFileWriter.writerow([self.ssn,self.name,self.role,self.rank,self.email,self.licence,self.address,self.mobile_phone_number,self.home_phone_number])
