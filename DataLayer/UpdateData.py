6be5c584bdfa1254eac28879fcce8b578daf87e4
SILLI20 <[sigvaldi19@ru.is]>
2019-12-12 15:18:50 +0000

    pulling

--- DataLayer/UpdateData.py
+++ DataLayer/UpdateData.py
@@ -24,22 +24,21 @@ class UpdateData:
         file_stream.close()
 
     def updateWorkTrip(self, data):
-    ''' This writes every value of the data into the crew file again, updating it. '''
-    with open("./STUDENTDATA/Flights.csv", 'w',newline='\n') as file_stream:
-        newFileWriter = csv.writer(file_stream)
-        newFileWriter.writerow(['flightNumber', 'departingFrom', 'arrivingAt', 'departure', 'arrivial', 'aircraftID', 'captain', 'copilot', 'fsm', 'fa1', 'fa2'])
-        for item in data:
-            self.flightNumber = item['flightNumber']
-            self.departingFrom = item['departingFrom']
-            self.arrivingAt = item['arrivingAt']
-            self.departure = item['departure']
-            self.arrival = item['arrival']
-            self.aircraftID = item['aircraftID']
-            self.captain = item['captain']
-            self.copilot = item['copilot']
-            self.fsm = item['fsm']
-            self.fa1 = item['fa1']
-            self.fa2 = item['fa2']
-            
-            newFileWriter.writerow([self.flightNumber,self.departingFrom,self.arrivingAt,self.departure,self.arrival,self.aircraftID,self.captain,self.copilot,self.fsm, self.fa1, self.fa2])
-    file_stream.close()
+        with open("./STUDENTDATA/Flights.csv", 'w',newline='\n') as file_stream:
+            newFileWriter = csv.writer(file_stream)
+            newFileWriter.writerow(['flightNumber', 'departingFrom', 'arrivingAt', 'departure', 'arrivial', 'aircraftID', 'captain', 'copilot', 'fsm', 'fa1', 'fa2'])
+            for item in data:
+                self.flightNumber = item['flightNumber']
+                self.departingFrom = item['departingFrom']
+                self.arrivingAt = item['arrivingAt']
+                self.departure = item['departure']
+                self.arrival = item['arrival']
+                self.aircraftID = item['aircraftID']
+                self.captain = item['captain']
+                self.copilot = item['copilot']
+                self.fsm = item['fsm']
+                self.fa1 = item['fa1']
+                self.fa2 = item['fa2']
+                
+                newFileWriter.writerow([self.flightNumber,self.departingFrom,self.arrivingAt,self.departure,self.arrival,self.aircraftID,self.captain,self.copilot,self.fsm, self.fa1, self.fa2])
+        file_stream.close()
